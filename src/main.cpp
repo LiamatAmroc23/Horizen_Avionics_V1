d10ddafd19df00c3ee2dd98bd6cd143a544f96ac
Liam Gessman <Liamngessman+frc@gmail.com>
2025-02-21 08:28:04 -0500

    added mpu6050

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
+#include <Adafruit_MPU6050.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
@@ -14,11 +15,10 @@ const int chipSelect = 10;
 
 const int mainChute = 6;
 const int drougeChute = 7;
-const int 
 
 const int mainDeploy = 300;
 
-float packet[6];
+float packet[12];
 //data packet array
 float cur;
 float prev;
@@ -36,6 +36,13 @@ float dis;
 int r;
 int i;
 int a;
+//mpu integers
+float gx;
+float gy;
+float gz;
+float ax;
+float ay;
+float az;
 
 int dig[4];
 
@@ -47,6 +54,7 @@ int average();
 
 
 Adafruit_BMP3XX bmp;
+Adafruit_MPU6050 mpu;
 
 void setup() {
   Serial.begin(9600);
@@ -57,10 +65,73 @@ void setup() {
     Serial.println("BMP390 Not found!");
     while(true);
   }
+  if(!mpu.begin()){
+    Serial.println("Failed to find MPU6050!");
+    while(true);
+  }
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_100_HZ);
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
 
   //tests sd card and BMP390
   Serial.print("Sampling...");
@@ -84,7 +155,6 @@ void setup() {
   Serial.println("Time(sec)  Temp(C)  Pressure(HPA)  Altitude (F) ");
   pinMode(drougeChute, OUTPUT);
   pinMode(mainChute, OUTPUT);
-  pinMode(BUILTIN_LED)
 }
 
 String arrayToString(float *array, int size) {
@@ -99,12 +169,11 @@ String arrayToString(float *array, int size) {
 
   return result;
 }
-int Pulse(int limit){
-
-
-}
 
 void loop() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  
   for(int a = 1; a < samp; a++){
     sum = sum + bmp.readAltitude(SEALEVELPRESSURE_HPA)*3.28 -altZ;
   }
@@ -115,6 +184,13 @@ void loop() {
   tim = tim + .1;
   dis = cur - prev;
   speed = dis / tim;
+
+  gx = g.gyro.x;
+  gy = g.gyro.y;
+  gz = g.gyro.z;
+  ax = a.acceleration.x;
+  ay = a.acceleration.y;
+  az = a.acceleration.z;
 
 
   if(cur > apogee){
@@ -140,6 +216,13 @@ void loop() {
   packet[3]=sum;
   packet[4]=apogee;
   packet[5]=speed;
+  packet[6]=gx;
+  packet[7]=gy;
+  packet[8]=gz;
+  packet[9]=ax;
+  packet[10]=ay;
+  packet[11]=az;
+
   int arraySize = sizeof(packet) / sizeof(packet[0]);
 
   String result = arrayToString(packet, arraySize);
